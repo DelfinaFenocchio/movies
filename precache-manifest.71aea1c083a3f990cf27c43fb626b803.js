69f195c4532661613cff4cbbda3ddaf236638714
DelfinaFenocchio <delfifenocchio@gmail.com>
2019-09-09 22:10:42 -0300

    Updates

---
+++ precache-manifest.71aea1c083a3f990cf27c43fb626b803.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "528b99cd3aa8c9bc488512e5e45d24fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b18d3d42100a3684bf0",
+    "url": "/static/css/main.ada031d6.chunk.css"
+  },
+  {
+    "revision": "01aafb586a833060b843",
+    "url": "/static/js/2.6960cc3e.chunk.js"
+  },
+  {
+    "revision": "7b18d3d42100a3684bf0",
+    "url": "/static/js/main.749a4779.chunk.js"
+  },
+  {
+    "revision": "33fdee5c870c0f0ebbd0",
+    "url": "/static/js/runtime~main.c066441f.js"
+  }
+]);
